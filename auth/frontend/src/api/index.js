85282c1c5a6f459a667a68749ed3edfbf46f93c5
Andrei Seritan <seritandrei@gmail.com>
2021-05-20 12:41:05 +0300

    [FRONTEND] Add comments for the exposed API

--- auth/frontend/src/api/index.js
+++ auth/frontend/src/api/index.js
@@ -1,51 +1,55 @@
-const LOGIN_API = "http://localhost:8004"
+const GATEWAY = "http://localhost:8004";
 
+/**
+ * Attempts to login the user identified by username and password.
+ *
+ * @param {string} username The users username.
+ * @param {string} password The usernames associated password.
+ * @return {string} Session token associated with the user.
+ * NOTE: on failure, the function throws an error.
+ */
 export async function login(username, password) {
-	try {
-		const response = await fetch(`${LOGIN_API}/auth/login`, {
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				username: username,
-				password: password,
-			})
-		})
+  const response = await fetch(`${GATEWAY}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
 
-		if (response.status !== 201) {
-			const errText = await response.text();
-			throw new Error(errText)
-		} else {
-			const token = await response.text();
-			return token;
-		}
-	} catch (e) {
-		throw e
-	}
+  if (response.status !== 201) {
+    const errText = await response.text();
+    throw new Error(errText);
+  } else {
+    const token = await response.text();
+    return token;
+  }
 }
 
+/**
+ * Attempts to register a new user with the provided credentials.
+ *
+ * @param {string} username The users username.
+ * @param {string} password The usernames associated password.
+ * NOTE: on failure, the function throws an error.
+ */
 export async function register(username, password) {
-	try {
-		const response = await fetch(`${LOGIN_API}/auth/register`, {
-			method: 'POST',
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				username: username,
-				password: password,
-			})
-		})
+  const response = await fetch(`${GATEWAY}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
 
-		if (response.status !== 201) {
-			const errText = await response.text();
-			throw new Error(errText)
-		} else {
-			const token = await response.text();
-			return token;
-		}
-	} catch (e) {
-		throw e
-	}
+  if (response.status !== 201) {
+    const errText = await response.text();
+    throw new Error(errText);
+  }
 }
