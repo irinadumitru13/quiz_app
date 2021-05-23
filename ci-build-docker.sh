7dc9eb153e4f4e71b0110908f1e97f8ecc2cc9bb
Andrei Seritan <seritandrei@gmail.com>
2021-05-23 12:09:46 +0300

    Add Docker image build script

---
+++ ci-build-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script for building docker images with gitlab CI
+
+DOCKERFILE=Dockerfile
+
+# Iterate through all services in the repository's root to build them
+for app in $(find . -mindepth 1 -maxdepth 1 -type d); do
+	if [ -f $app/$DOCKERFILE ]; then
+		cd $app
+
+		docker build -t registry.gitlab.com/idp-2021/quiz-app/$app .
+		docker push registry.gitlab.com/idp-2021/quiz-app/$app:latest
+
+		# Exit with failure if the previous command didn't run successfully
+		if [ $? -ne 0 ]; then
+			echo "Failed to build $app"
+			exit 2
+		fi
+
+		cd -
+	fi
+done
